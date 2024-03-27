ad45e9a5498698e5ce1d36433804ad5b028fe74c
Dennis Wolfers <dennis.wolfers@gmail.com>
2024-03-27 13:24:03 -0700

    New Grid button prompts user for grid size and creates a new grid.

--- grid-controller.js
+++ grid-controller.js
@@ -4,5 +4,25 @@ var gridSize = 16;
 for (let i = 0; i < (gridSize * gridSize); i++) {
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
+    pixel.addEventListener("mouseover", function() {
+        this.classList.add("touched");
+    });
     gridContainer.appendChild(pixel);
+}
+
+function newGrid() {
+    gridSize = prompt("Enter grid size");
+    gridSize = gridSize < 101 ? gridSize : 100;
+    const pixelSize = 960/gridSize;
+    gridContainer.innerHTML = "";
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixel.style.width = `${pixelSize}px`;
+        pixel.style.height = `${pixelSize}px`;
+        pixel.addEventListener("mouseover", function() {
+            this.classList.add("touched");
+        });
+        gridContainer.appendChild(pixel);
+    }
 }
